c3d7ee625696dd4c59179b2da12a3aac1d835b87
Tymofii M. <82289686+TymofiiZuren@users.noreply.github.com>
2026-03-25 16:27:18 +0000

    removed Payments link

--- StockPage/sidebar_stock.php
+++ StockPage/sidebar_stock.php
@@ -5,6 +5,5 @@
     <a href="staff.php" class="">Staff</a>
     <a href="players.php" class="">Players</a>
     <a href="competitions.php" class="">Competitions</a>
-    <a href="payments.php" class="">Payments</a>
     <a href="addStockItem.php" class="">Stock</a>
 </div>
